19766d1d9c161371739787113388ae187b46560b
Tharindu Rewatha <60292980+TharinduRewatha@users.noreply.github.com>
2021-03-10 10:13:42 +0530

    v_0.1.1

---
+++ _0_Custom/scripts/player/generalmanager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class generalmanager : MonoBehaviour
+{
+    public Animator obsAnim;
+
+    [Header("Attachments")]
+    public TextMeshProUGUI max;
+    public TextMeshProUGUI time;
+    public GameObject lostPanel;
+    public GameObject winpanel;
+
+    [Header("Generals")]
+    public float timeTowin;
+
+    private float currentTime;
+    private float startTime;
+    private float timeSpent;
+
+    public bool won;
+    public bool isGameOver;
+
+    void Start()
+    {
+        startTime = Time.time;
+        currentTime = Time.time;
+
+        timeSpent = currentTime - startTime;
+
+        isGameOver = false;
+        won = false;
+
+        max.text = timeTowin.ToString();
+    }
+
+    void Update()
+    {
+        currentTime = Time.time;
+
+        if (timeSpent < timeTowin)
+        {
+            timeSpent = currentTime - startTime;
+            time.text = timeSpent.ToString("f0");
+        }
+        else
+        {
+            if (!won && !isGameOver)
+            {
+                Vector2 pos = new Vector2(0,0);
+                Quaternion rot = Quaternion.Euler(0,0,0);
+                GameObject go = Instantiate(winpanel,pos,rot);
+            }
+
+            won = true;
+            isGameOver = true;
+        }
+
+        obsAnim.speed = currentTime/timeTowin + 0.3f;
+    }
+}
---
+++ _0_Custom/scripts/player/playercontroller.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    [Header("Attachments")]
+    public Animator anim;
+
+    public float gravity = -10f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    private Vector3 velocity;
+    public bool isGrounded;
+
+    private Rigidbody rb;
+    private generalmanager generalmanager;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        generalmanager = GameObject.FindObjectOfType<generalmanager>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        rb.velocity = velocity;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+            anim.SetBool("isJumping",false);
+        }
+
+
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        if (Input.touchCount > 0 && isGrounded)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                jump();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "obs")
+        {
+            Destroy(gameObject);
+
+            Vector2 pos = new Vector2(0, 0);
+            Quaternion rot = Quaternion.Euler(0, 0, 0);
+            GameObject go = Instantiate(generalmanager.lostPanel, pos, rot);
+
+            generalmanager.isGameOver = true;
+            generalmanager.won = false;
+        }
+    }
+
+    private void jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        anim.SetBool("isJumping", true);
+    }
+}
